ce49b5127753d9da230bdcff641d9b3d046e9b1c
jihyean <jhkwon@moasoftware.co.kr>
2024-12-22 13:47:23 +0900

    [feat] postgresql로 DB 변경

--- back/config/settings.py
+++ back/config/settings.py
@@ -91,10 +91,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+# postgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cherrypicker', # DB명
+		'USER': 'postgres',
+		'PASSWORD': '1234',
+		'HOST': 'localhost',
+		'PORT': '5432',
     }
 }
 
