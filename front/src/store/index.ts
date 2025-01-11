7346f8d489611f518743b6d6a8e32aa36be8b12b
jihyean <d9rmlatte@gmail.com>
2025-01-11 22:40:38 +0900

    [fix] createNoopStorage - 헤당 오류 수정 - next.js에서만 발생하는 에러 redux-persist failed to create sync storage. falling back to noop storage.

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -10,7 +10,29 @@ import {
   PURGE,
   REGISTER,
 } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
+
+const createNoopStorage = () => {
+  return {
+    getItem(_key: any) {
+      return Promise.resolve(null);
+    },
+    setItem(_key: any, value: any) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key: any) {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage =
+  typeof window === 'undefined'
+    ? createNoopStorage()
+    : createWebStorage('local');
+
 
 const authPersistConfig = {
   key: 'auth',
